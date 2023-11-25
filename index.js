71248d682912e814142c32244d2bede23106ba66
Shubh Gupta <set1st2c@gmail.com>
2023-11-25 14:58:02 +0530

    making modules

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('sup bb');
+
+const person = require('./person');
+
+const person1 = new person('John Doe',26);
+person1.greeting();
+
+console.log(person);
---
+++ person.js
@@ -0,0 +1,16 @@
+/*const person = {
+        name : 'Shubh',
+        age : 18
+};*/
+
+class Person {
+        constructor(name,age) {
+                this.name=name
+                this.age=age
+        }
+        greeting () {
+                console.log(`My name is ${this.name} and I am ${this.age} `);
+        }
+}
+
+module.exports = Person;
