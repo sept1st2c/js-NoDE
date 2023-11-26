422d59762dd2618d3732b78e02a32eedba8bf468
Shubh Gupta <set1st2c@gmail.com>
2023-11-26 17:59:02 +0530

    cool reference

---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require("os");
+
+//platform
+console.log(os.platform());
+
+//cpu arch
+console.log(os.arch());
+
+//cpu core info
+console.log(os.cpus());
+
+// free memorry
+console.log(os.freemem());
+
+// total memory
+console.log(os.totalmem());
+
+//home dir
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
